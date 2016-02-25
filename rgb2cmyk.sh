df8ef5c5dc8a2761d37cec594801fb1118882cdc
Stéphanie Vilayphiou <stephanie@stdin.fr>
2016-02-25 13:02:23 +0100

    Update rgb2cmyk.sh to preserve true black, with overprint option.

--- rgb2cmyk.sh
+++ rgb2cmyk.sh
@@ -1,9 +1,34 @@
-#! /usr/bin/env bash
-#
+#! /bin/bash
+
+# CONVERTS RGB PDF TO CMYK PDF PRESERVING TRUE BLACK FOR VECTORS
+# OVERPRINT FEATURE IS AVAILABLE WITH AN OPTION
+
 # USAGE:
 # ./rgb2cmyk.sh input.pdf
-#
+# ./rgb2cmyk.sh input.pdf overprint
+
 # OUTPUT:
 # input-cmyk.pdf
+# input-cmyk-op.pdf (in case of overprint)
 
-gs -dNOPAUSE -sDEVICE=pdfwrite -dGraphicKPreserve=2  -sColorConversionStrategy=CMYK -dProcessColorModel=/DeviceCMYK -dNOPAUSE -dBATCH  -sOUTPUTFILE=$(basename ${1} .pdf)-cmyk.pdf ${1}
+bn=$(basename $1 ".pdf")
+op=$2
+
+pdftops -level2sep ${bn}.pdf
+
+# IF YOU STILL HAVE SOME CMYK BLACK, TRY (UN)COMMENTING LINE 7 AND 8
+#gs -dNOPAUSE -dBATCH -sDEVICE=pdfwrite -sOutputFile=${bn}-cmyk.pdf ./forceblack.ps ${bn}.ps
+gs -dNOPAUSE -sDEVICE=pdfwrite -dGraphicKPreserve=2  -sColorConversionStrategy=CMYK -dProcessColorModel=/DeviceCMYK -dNOPAUSE -dBATCH  -sOUTPUTFILE=${bn}-cmyk.pdf ${bn}.ps
+
+if [ $op == overprint ]
+    then
+        pdftops -level2sep ${bn}-cmyk.pdf
+        sed -e "s/false \(op\)/true \1/gI" ${bn}-cmyk.ps > ${bn}-cmyk-op.ps
+        gs -dNOPAUSE -dBATCH -sDEVICE=pdfwrite -sOutputFile=${bn}-cmyk-op.pdf ./forceblack.ps ${bn}-cmyk-op.ps
+        rm ${bn}-cmyk.pdf
+        rm ${bn}-cmyk.ps
+fi        
+
+rm ${bn}.ps
+
+
